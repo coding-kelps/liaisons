4f3f631e97a4a47028aa62b5498f6386db16e6a7
guilhem-sante <76043368+guilhem-sante@users.noreply.github.com>
2024-06-15 21:11:25 +0100

    refactor: rename "retrieve-arguments" subcommand into "summarize-arguments"

--- src/clients.rs
+++ src/clients.rs
@@ -5,10 +5,21 @@ use thiserror::Error as ThisError;
 use serde_yaml::Mapping;
 use crate::models;
 
+/// An abstraction of a Large Language Model client (e.g., Ollama), to provide 
+/// an interface to requests arguments summarization and relations prediction.
+/// 
+/// # Examples
+/// 
+/// ```rs
+/// let client = Client::new("/path/to/client/config.yml");
+/// 
+/// let summarized_argument = client.summarize("An example argument").unwrap();
+/// ```
 pub enum Client {
     Ollama(ollama::Client),
 }
 
+/// The different kinds of error expected from a client.
 #[derive(Debug, ThisError)]
 pub enum Error {
     #[error("IO error: {0}")]
@@ -26,16 +37,42 @@ pub enum Error {
 }
 
 impl Client {
+    /// Create a new Large Language Model client from a YAML configuration file.
+    /// For now the only accepted configuration is a configuration for an Ollama
+    /// client.
+    /// 
+    /// # Arguments
+    /// 
+    /// * `path` - The path of the client YAML configuration file.
+    /// 
+    /// # Errors
+    /// 
+    /// This function will return an error if:
+    /// 
+    /// * The configuration file does not exist.
+    /// * The file cannot be read.
+    /// * The file contains invalid data (e.g., wrong format)
+    /// * There is no or more than one client configuration in the file.
+    /// 
+    /// # Examples
+    /// 
+    /// ```rs
+    /// let client = Client::new("/path/to/client/config.yml");
+    /// 
+    /// let summarized_argument = client.summarize("An example argument").unwrap();
+    /// ```
     pub fn new(path: & PathBuf) -> Result<Self, Error> {
-        let data = fs::read_to_string(path)?;
+        // Retrieve file as mapping to identify which type client configuration
+        // is loaded from the available client keys.
+        let mapping = {
+            let data = fs::read_to_string(path)?;
 
-        let mapping = serde_yaml::from_str::<Mapping>(&data)?;
-
+            serde_yaml::from_str::<Mapping>(&data)?
+        };
         let mut loaded_client: Option<Self> = None;
-        let client_keys = ["ollama"];
+        let accepted_client_keys = ["ollama"];
 
-
-        for key in client_keys {
+        for key in accepted_client_keys {
             if mapping.get(key).is_some() {
                 let value = mapping.get(key).unwrap();
 
@@ -55,19 +92,23 @@ impl Client {
 
         match loaded_client {
             Some(client) => Ok(client),
-            None => Err(Error::Configuration(String::from(""))),
+            None => Err(Error::Configuration(String::from("no client configuration found"))),
         }
     }
 }
 
+/// Trait defining all the Large Language Model client expected features for
+/// this program.
 pub trait ClientTrait {
-    async fn retrieve(&self, content: String) -> Result<models::Argument, Error>;
+    /// Summarize the underlying argument of a user generated web-content
+    /// (e.g., Twitter Post)
+    async fn summarize(&self, content: String) -> Result<models::Argument, Error>;
 }
 
 impl ClientTrait for Client {
-    async fn retrieve(&self, content: String) -> Result<models::Argument, Error> {
+    async fn summarize(&self, content: String) -> Result<models::Argument, Error> {
         match self {
-            Client::Ollama(client) => client.retrieve(content).await,
+            Client::Ollama(client) => client.summarize(content).await,
         }
     }
 }
