e5a312f5748c7ec3b955a45e5a2d109a749ef81c
guilhem-sante <76043368+guilhem-sante@users.noreply.github.com>
2024-06-21 19:12:55 +0100

    feat: add retrieve_argument feature to neo4j repository

--- src/models/argument.rs
+++ src/models/argument.rs
@@ -3,6 +3,7 @@ use serde::{Deserialize, Serialize};
 /// Represents an argument.
 #[derive(Serialize, Deserialize, Clone)]
 pub struct Argument {
+    pub id: Option<u32>,
     pub summarized_info: SummarizedInfo,
     /// The user-generated web content from which the argument was extracted
     /// as a raw string.
@@ -19,6 +20,15 @@ pub struct SummarizedInfo {
 impl Argument {
     pub fn new(summarized_info: SummarizedInfo, raw: String) -> Self {
         Self {
+            id: None,
+            summarized_info: summarized_info,
+            raw: raw,
+        }
+    }
+
+    pub fn with_id(id: u32, summarized_info: SummarizedInfo, raw: String) -> Self {
+        Self {
+            id: Some(id),
             summarized_info: summarized_info,
             raw: raw,
         }
--- src/subcommands/predict.rs
+++ src/subcommands/predict.rs
@@ -1,6 +1,9 @@
 use thiserror::Error as ThisError;
 use crate::configuration::settings::{LLMCfg, RepositoryCfg, Prompt};
 use crate::clients::repository;
+use crate::clients::repository::RepositoryTrait;
+use crate::models;
+
 
 #[derive(Debug, ThisError)]
 pub enum Error {
@@ -18,11 +21,19 @@ pub struct PredictRelationCfg {
     pub llm_cfg: LLMCfg,
     pub repo_cfg: RepositoryCfg,
     pub prompt: Prompt,
+    pub args_id: Vec<u32>,
 }
 
 /// Draft of the comming features for argument relations prediction.
-#[allow(unused_variables)]
 pub async fn predict_relations(cfg: PredictRelationCfg) -> Result<(), Error> {
+    let mut repo_client = repository::Repository::new(&cfg.repo_cfg)
+        .await?;
+    let mut args = Vec::<models::Argument>::with_capacity(cfg.args_id.len());
+
+    for arg_id in cfg.args_id {
+        args.push(repo_client.retrieve_argument(arg_id).await?);
+    }
+
     Ok(())
 }
 
