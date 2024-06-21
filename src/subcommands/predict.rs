36aede27cb37ae639f34893e50ede83f67d1cc10
guilhem-sante <76043368+guilhem-sante@users.noreply.github.com>
2024-06-21 17:03:54 +0100

    feat: add error handling at subcommand level

--- src/subcommands/predict.rs
+++ src/subcommands/predict.rs
@@ -1,11 +1,28 @@
-use std::path::PathBuf;
-use tokio::fs;
+use thiserror::Error as ThisError;
+use crate::configuration::settings::{LLMCfg, RepositoryCfg, Prompt};
+use crate::clients::repository;
+
+#[derive(Debug, ThisError)]
+pub enum Error {
+    #[error("io error: {0}")]
+    IOError(#[from] std::io::Error),
+
+    #[error("serialization error: {0}")]
+    SerializationError(#[from] serde_json::Error),
+
+    #[error("repository error: {0}")]
+    RepositoryError(#[from] repository::Error),
+}
+
+pub struct PredictRelationCfg {
+    pub llm_cfg: LLMCfg,
+    pub repo_cfg: RepositoryCfg,
+    pub prompt: Prompt,
+}
 
 /// Draft of the comming features for argument relations prediction.
 #[allow(unused_variables)]
-pub async fn predict_relations(file_path: PathBuf) -> Result<(), ()> {
-    let _data = fs::read_to_string(file_path).await.unwrap();
-
+pub async fn predict_relations(cfg: PredictRelationCfg) -> Result<(), Error> {
     Ok(())
 }
 
