33b55045f73c4b828141eef717fbbcd9dbbe1489
Guilhem Santé <guilhem.snt@gmail.com>
2024-06-27 02:35:00 +0100

    feat: add new logger

--- src/configuration/settings.rs
+++ src/configuration/settings.rs
@@ -1,9 +1,10 @@
+pub use config;
 use config::{ConfigBuilder, ConfigError, File, FileFormat};
 use config::builder::DefaultState;
-use serde::Deserialize;
+use serde::{Serialize, Deserialize};
 use crate::configuration::Cli;
 
-#[derive(Debug, Deserialize, Clone)]
+#[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct Settings {
     pub log: Log,
     pub llm: LLMCfg,
@@ -11,70 +12,69 @@ pub struct Settings {
     pub prompts: Prompts,
 }
 
-#[derive(Debug, Deserialize, Clone)]
+#[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct Log {
     pub level: String,
 }
 
-#[derive(Debug, Deserialize, Clone)]
+#[derive(Debug, Serialize, Deserialize, Clone)]
 pub enum LLMCfg {
     Ollama(OllamaCfg)
 }
 
-#[derive(Debug, Deserialize, Clone)]
+#[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct OllamaCfg {
     pub uri: String,
     pub model: String,
 }
 
-#[derive(Debug, Deserialize, Clone)]
+#[derive(Debug, Serialize, Deserialize, Clone)]
 pub enum RepositoryCfg {
     Neo4j(Neo4jCfg),
 }
 
-#[derive(Debug, Deserialize, Clone)]
+#[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct Neo4jCfg {
     pub uri: String,
     pub user: String,
     pub password: String,
 }
 
-#[derive(Debug, Deserialize, Clone)]
+#[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct Prompts {
     pub summary: Prompt,
     pub predict: Prompt,
 }
 
-#[derive(Debug, Deserialize, Clone)]
+#[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct Prompt {
     pub system: Option<String>,
     pub prompt: String,
 }
 
-// TODO: change this to /etc/liaisons/default when client is ready to run as a
-// job.
-const DEFAULT_CFG_PATH: &str = ".dev/config/default";
-
 impl Settings {
-    pub fn new(config_path: & Option<String>) -> Result<Self, ConfigError> {
+    pub fn load_from_file(path: String) -> Result<Self, ConfigError> {
         let builder: ConfigBuilder<DefaultState> = ConfigBuilder::default();
 
-        let mut builder = builder
-            .add_source(File::new(DEFAULT_CFG_PATH, FileFormat::Yaml));
-
-        if let Some(ref path) = config_path {
-            builder = builder
-                .add_source(File::new(path, FileFormat::Yaml));
-        }
+        let builder = builder
+            .add_source(File::new(path.as_str(), FileFormat::Yaml));
         
         let config = builder.build()?;
         
         config.try_deserialize()
     }
 
-    pub fn merge_to_cli(& mut self, cli: &Cli) {
-        if let Some(ref level) = cli.log.level {
-            self.log.level = level.clone();
+    pub fn merge_to_cli(&self, cli: &Cli) -> Self {
+        Settings {
+            log: Log {
+                level: match &cli.log.level {
+                    Some(level) => level.to_string(),
+                    None => self.log.level.clone(),
+                }
+            },
+            llm: self.llm.clone(),
+            repository: self.repository.clone(),
+            prompts: self.prompts.clone(),
         }
     }
 }
